5af16d8aedb2a0d491f26b75faf6044490f9d5ea
Amirreza H <itsamirrezah@gmail.com>
2023-04-18 17:56:11 +0330

    add button for login/signup and import new font

---
+++ src/components/round-button/round-button.tsx
@@ -0,0 +1,6 @@
+import { ButtonHTMLAttributes } from "react";
+import styles from "./round-button.module.css"
+
+export default function RoundButton({children}: ButtonHTMLAttributes<HTMLButtonElement>){
+   return <button className={styles.button}>{children}</button>; 
+}
