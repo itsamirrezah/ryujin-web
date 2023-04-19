3131ac2f55a12f4da11d7a43ab730255d60169ef
Amirreza H <itsamirrezah@gmail.com>
2023-04-19 21:49:27 +0330

    initiate home layout

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,16 +1,31 @@
-import RoundButton from "@/components/round-button/round-button"
 import LogoMasters from "@/components/logo/logo-masters"
+import LogoPrimary from "@/components/logo/logo-primary"
+import LogoSecondary from "@/components/logo/logo-secondary"
+import RoundButton from "@/components/round-button/round-button"
+import styles from "./home.module.css"
 
 export default function HomePage(){
   return (
     <div>
-      <h1>Build Ryujin web-app with Vite, React and Typescript.</h1>
-      <RoundButton>Sign Up</RoundButton>
-      <RoundButton>Login</RoundButton>
-      <div>
-        <LogoMasters/>
-      </div>
-      <p>hello world</p>
+        <div className={styles.main}>
+            <div>
+                <div style={{width: 400}}>
+                    <LogoPrimary/>
+                </div>
+                <div style={{width: 400, marginTop: -10}}>
+                    <LogoSecondary/>
+                </div>
+            <h3>Play online and challenge your friends</h3>
+            </div>
+            <div>
+                <LogoMasters/>
+                <RoundButton>Sign Up</RoundButton>
+                <RoundButton>Login</RoundButton>
+            </div>
+        </div>
+        <div>
+            <h1>Section 2</h1>
+        </div>
     </div>
   )
 }
