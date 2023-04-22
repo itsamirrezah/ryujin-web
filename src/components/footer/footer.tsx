672a89e941c4aa8dc784133f5b27ec9287ed5ca4
Amirreza H <itsamirrezah@gmail.com>
2023-04-22 10:20:17 +0330

    add footer

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,5 @@
+import styles from "./footer.module.css"
+
+export default function Footer(){
+    return <footer className={styles.footer}><small>Ryujin is a open-source multiplayer game under the AGPL licence.</small></footer>
+}
--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,12 +1,14 @@
 import Header from "../header/header"
 import styles from "./layout.module.css"
 import { Outlet } from "@tanstack/router"
+import Footer from "../footer/footer"
 
 export default function Layout(){
     return (
             <div className={`${styles.container} ${styles.home}`}>
                 <Header />
                 <main><Outlet/></main> 
+                <Footer />
             </div>
    )
 }
