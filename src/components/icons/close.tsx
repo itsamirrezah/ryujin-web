fa8659abe0cb578466369cf27bb68a7fe6261e9e
Amirreza H <itsamirrezah@gmail.com>
2023-04-23 14:14:41 +0330

    add google and close icons

---
+++ src/components/icons/close.tsx
@@ -0,0 +1,8 @@
+export default function Close(){
+    return (
+        <svg width="39" height="39" viewBox="0 0 39 39" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M25.8171 12.8906L12.5891 26.1094M12.5891 12.8906L25.8171 26.1094" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M19.203 37.8594C29.3497 37.8594 37.5753 29.6396 37.5753 19.5C37.5753 9.3604 29.3497 1.14062 19.203 1.14062C9.05634 1.14062 0.830811 9.3604 0.830811 19.5C0.830811 29.6396 9.05634 37.8594 19.203 37.8594Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+    )
+}
