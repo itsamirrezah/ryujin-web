31bbd364bfae26c302d4535da89f953ac5a2ddd5
Amirreza H <itsamirrezah@gmail.com>
2023-05-31 12:52:48 +0330

    initiate play page

--- src/lib/router.tsx
+++ src/lib/router.tsx
@@ -1,34 +1,36 @@
 import Layout from "@/components/layout/layout";
 import HomePage from "@/pages/home";
+import PlayPage from "@/pages/play";
 import { RootRoute, Router, Route } from "@tanstack/router";
 
 declare module '@tanstack/router' {
-  interface Register {
-    router: typeof router
-  }
+    interface Register {
+        router: typeof router
+    }
 }
 
-const root = new RootRoute({component: Layout});
+const root = new RootRoute({ component: Layout });
 
 const indexRoute = new Route({
-    getParentRoute: ()=> root,
+    getParentRoute: () => root,
     path: '/',
     component: HomePage
 })
 
 const playRoute = new Route({
-    getParentRoute: ()=> root,
-    path: '/play'
+    getParentRoute: () => root,
+    path: '/play',
+    component: PlayPage
 })
 
 const rulesRoute = new Route({
-    getParentRoute: ()=> root,
-    path: "/rules" 
+    getParentRoute: () => root,
+    path: "/rules"
 })
 
-const aboutRoute= new Route({
-    getParentRoute: ()=> root,
-    path: "/about" 
+const aboutRoute = new Route({
+    getParentRoute: () => root,
+    path: "/about"
 })
 
 const routeTree = root.addChildren([indexRoute, playRoute, rulesRoute, aboutRoute])
---
+++ src/pages/play.tsx
@@ -0,0 +1,11 @@
+import Board from "@/components/board/board";
+
+export default function PlayPage() {
+    return (
+        <div>
+            <h1>Play</h1>
+            <p>Play page!</p>
+            <Board/>
+        </div>
+    )
+}
