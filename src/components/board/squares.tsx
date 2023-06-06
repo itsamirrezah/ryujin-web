8a8eb7dc3797190ae395ac4bc0e267ee408409fd
Amirreza H <itsamirrezah@gmail.com>
2023-06-06 12:49:15 +0330

    refactor board to use grid

--- src/components/board/squares.tsx
+++ src/components/board/squares.tsx
@@ -8,23 +8,21 @@ import styles from "./squares.module.css"
 export default function Sqaures() {
     const { position, playerView } = useBoard()
     return (
-        <div className={styles.board} style={{ display: "flex", flexDirection: "column", gap: 4, width: "100%", height: "100%" }}>
-            {[...Array(BOARD_SIZE)].map((_, i) => {
+        <div className={styles.board} style={{
+            display: "grid", gridTemplateRows: "repeat(5, 1fr)", gridTemplateColumns: "repeat(5,1fr)",
+        }}>
+            {[...Array(BOARD_SIZE * BOARD_SIZE)].map((_, i) => {
+                const row = Math.floor(i / BOARD_SIZE)
+                const col = i % BOARD_SIZE
+                const square = playerView === "w" ?
+                    COLUMNS[col] + (BOARD_SIZE - row) as SquareType :
+                    COLUMNS[BOARD_SIZE - 1 - col] + (row + 1) as SquareType
+                const piece = position[square]
+                const squareColor = row % 2 === col % 2 ? "white" : "black"
                 return (
-                    <div className="row" key={i} style={{ display: "flex", gap: 4, flex: 1 }}>
-                        {[...Array(BOARD_SIZE)].map((_, j) => {
-                            const square = playerView === "w" ?
-                                COLUMNS[j] + (BOARD_SIZE - i) as SquareType :
-                                COLUMNS[BOARD_SIZE - 1 - j] + (i + 1) as SquareType
-                            const piece = position[square]
-                            const squareColor = i % 2 === j % 2 ? "white" : "black"
-                            return (
-                                <Square key={(i * BOARD_SIZE) + j} square={square} color={squareColor}>
-                                    {piece && <Piece piece={piece} square={square} />}
-                                </Square>
-                            )
-                        })}
-                    </div>
+                    <Square key={i} square={square} color={squareColor}>
+                        {piece && <Piece piece={piece} square={square} />}
+                    </Square>
                 )
             })}
         </div>
