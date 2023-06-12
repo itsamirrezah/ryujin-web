09d7a2693c825862e17d6e9a42998681c5dc51e4
Amirreza H <itsamirrezah@gmail.com>
2023-06-12 12:20:34 +0330

    add socket.io dependency & simple join room functionality

--- src/pages/play.tsx
+++ src/pages/play.tsx
@@ -1,8 +1,30 @@
 import Board from "@/components/board/board";
 import RoundButton from "@/components/round-button/round-button";
+import { socket } from "@/lib/socket";
+import { useEffect, useState } from "react";
 import styles from "./play.module.css";
 
 export default function PlayPage() {
+    const [isConnected, setIsConnected] = useState<boolean>(socket.connected);
+    useEffect(() => {
+        if (!socket.connected) {
+            socket.connect();
+        }
+        socket.on("connect", () => {
+            setIsConnected(true);
+        })
+        socket.on("disconnect", () => {
+            setIsConnected(false);
+        })
+        socket.on("JOIN_ROOM", (data) => {
+            console.log("JOIN_ROOM", data);
+        })
+
+    }, [])
+
+    function joinRoom() {
+        socket.emit("CREATE_OR_JOIN_ROOM");
+    }
     return (
         <div className={styles.main}>
             <div className={styles.game}>
@@ -10,7 +32,7 @@ export default function PlayPage() {
                     <Board />
                 </div>
                 <div className={styles.side}>
-                    <RoundButton>New opponent</RoundButton>
+                    <RoundButton onClick={joinRoom}>New opponent</RoundButton>
                 </div>
             </div>
         </div >
