4b260da8ad31b696ee8d465cb58ea37f96fc06d4
Amirreza H <itsamirrezah@gmail.com>
2023-07-10 15:50:21 +0330

    display cards when game started

--- src/pages/play.tsx
+++ src/pages/play.tsx
@@ -2,10 +2,11 @@ import Board from "@/components/board/board";
 import { usePlay } from "@/lib/play/play-context";
 import RoundButton from "@/components/round-button/round-button";
 import styles from "./play.module.css";
-import { PieceType, SquareType } from "@/components/board/types";
+import { PieceType } from "@/components/board/types";
+import Card from "@/components/card/card";
 
 export default function PlayPage() {
-    const { joinRoom, hasRoom, roomId, boardPosition, selfColor, hasTurn } = usePlay()
+    const { joinRoom, hasRoom, roomId, boardPosition, selfColor, hasTurn, isGameStarted, selfCards, opponentCards } = usePlay()
 
     function isAllowedToMove(piece: PieceType) {
         return hasTurn && piece[0] === selfColor;
@@ -18,8 +19,16 @@ export default function PlayPage() {
                     <Board position={boardPosition} isAllowedToMove={isAllowedToMove} />
                 </div>
                 <div className={styles.side}>
-                    {!hasRoom && <RoundButton onClick={joinRoom}>New opponent</RoundButton>}
-                    {hasRoom && <span>{`game started @${roomId}`}</span>}
+                    {!hasRoom && !isGameStarted && <RoundButton onClick={joinRoom}>New opponent</RoundButton>}
+                    {isGameStarted && <div className={styles.cards}>
+                        <div className={styles.cardsuser}>
+                            {opponentCards?.map(c => <Card card={c} />)}
+                        </div>
+                        <Card />
+                        <div className={styles.cardsuser}>
+                            {selfCards?.map(c => <Card card={c} />)}
+                        </div>
+                    </div>}
                 </div>
             </div>
         </div >
