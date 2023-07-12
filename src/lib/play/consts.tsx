3d3f545cf6cd2ed2109eabff4449dd2c3ff88503
Amirreza H <itsamirrezah@gmail.com>
2023-07-12 10:51:53 +0330

    calculate coordinate deviation of each card options

--- src/lib/play/consts.tsx
+++ src/lib/play/consts.tsx
@@ -1,24 +1,41 @@
 
 export type Card = {
     name: string,
-    options: number[];
+    options: number[],
+    delta: Delta[];
 }
 
+type Delta = {
+    x: number,
+    y: number
+}
+
+function getDelta(options: number[]) {
+    const BOARD_WIDTH = 5
+    const delta = [] as Delta[]
+    for (let i = 0; i < options.length; i++) {
+        const opt = options[i]
+        const y = Math.floor(opt / BOARD_WIDTH)
+        const x = opt % BOARD_WIDTH
+        delta.push({ x, y })
+    }
+    return { options, delta }
+}
 export const cards: Card[] = [
-    { name: "tiger", options: [2, 17] },
-    { name: "cobra", options: [8, 11, 18] },
-    { name: "dragon", options: [5, 9, 16, 18] },
-    { name: "rabbit", options: [8, 14, 16] },
-    { name: "crab", options: [7, 10, 14] },
-    { name: "elephant", options: [6, 8, 11, 13] },
-    { name: "frog", options: [6, 10, 18] },
-    { name: "goose", options: [6, 11, 13, 18] },
-    { name: "rooster", options: [8, 11, 13, 16] },
-    { name: "monkey", options: [6, 8, 16, 18] },
-    { name: "mantis", options: [6, 8, 17] },
-    { name: "ox", options: [7, 13, 17] },
-    { name: "horse", options: [7, 11, 17] },
-    { name: "crane", options: [7, 16, 18] },
-    { name: "boar", options: [7, 11, 13] },
-    { name: "eel", options: [6, 13, 16] },
+    { name: "tiger", ...getDelta([2, 17]) },
+    { name: "cobra", ...getDelta([8, 11, 18]) },
+    { name: "dragon", ...getDelta([5, 9, 16, 18]) },
+    { name: "rabbit", ...getDelta([8, 14, 16]) },
+    { name: "crab", ...getDelta([7, 10, 14]) },
+    { name: "elephant", ...getDelta([6, 8, 11, 13]) },
+    { name: "frog", ...getDelta([6, 10, 18]) },
+    { name: "goose", ...getDelta([6, 11, 13, 18]) },
+    { name: "rooster", ...getDelta([8, 11, 13, 16]) },
+    { name: "monkey", ...getDelta([6, 8, 16, 18]) },
+    { name: "mantis", ...getDelta([6, 8, 17]) },
+    { name: "ox", ...getDelta([7, 13, 17]) },
+    { name: "horse", ...getDelta([7, 11, 17]) },
+    { name: "crane", ...getDelta([7, 16, 18]) },
+    { name: "boar", ...getDelta([7, 11, 13]) },
+    { name: "eel", ...getDelta([6, 13, 16]) },
 ]
