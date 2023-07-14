cf05cd99030b27bcca5121f359ac7ad8b55affed
Amirreza H <itsamirrezah@gmail.com>
2023-07-14 13:58:25 +0330

    simplify card by removing redundants & move inline styles to css module

--- src/components/card/card-board.tsx
+++ src/components/card/card-board.tsx
@@ -1,4 +1,4 @@
-import { BOARD_SIZE, COLUMNS } from "../board/consts"
+import { BOARD_SIZE } from "../board/consts"
 import styles from "./card-board.module.css"
 
 type CardBoardProps = {
@@ -10,15 +10,12 @@ export default function CardBoard({ options }: CardBoardProps) {
         <div className={styles.board}>
             {
                 [...Array(BOARD_SIZE * BOARD_SIZE)].map((_, i) => {
-                    const row = Math.floor(i / BOARD_SIZE)
-                    const col = i % BOARD_SIZE
-                    // const square = COLUMNS[col] + (BOARD_SIZE - row) as SquareType
-                    const squareColor = row % 2 === col % 2 ? "white" : "black"
-                    const isCenter = row === 2 && col === 2;
+                    const squareColor = i % 2 == 0 ? "white" : "black"
+                    const isCenter = i === Math.floor(BOARD_SIZE * BOARD_SIZE / 2)
                     return (
                         <div key={i} className={`${styles.sqaure} ${styles[`bg-${squareColor}`]}`}>
-                            {isCenter && <div style={{ backgroundColor: "#303456", width: "100%", height: "100%" }}></div>}
-                            {options.includes(i) && <div style={{ backgroundColor: "#C00C0CD1", width: "100%", height: "100%" }}></div>}
+                            {isCenter && <span className={`${styles.option} ${styles.center}`}></span>}
+                            {options.includes(i) && <span className={styles.option}></span>}
                         </div>
                     )
                 })
