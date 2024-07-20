3d83c107b06f406bee5dcc2e8b1bfc8dd254cdff
Amirreza H. <itsamirrezah@gmail.com>
2024-07-20 19:12:00 +0330

    initialize game for playing with computer
    * the `use-play-with-computer` hook now listens for the `waitingForComputer`
      state, updates players, and start the game independently.
    * introduced constansts and functions to handle the game
      initialization, which are managed by the server in online mode.
    * add `BOARD_WIDTH` and `DEFAULT_POSITION` and `ALL_CARDS` constants.
    * add  `shuffleCards` and `getDelta` functions.

--- src/lib/play/consts.tsx
+++ src/lib/play/consts.tsx
@@ -1,4 +1,5 @@
 import { assign, AssignAction } from "xstate";
+import { GameInfo } from "../socket";
 import {
     BlackOrWhite,
     CardType,
@@ -12,6 +13,7 @@ import {
     NavigateBackEvent,
     NavigateForwardEvent,
     OpponentMoveEvent,
+    PlayerResponse,
     Position,
     SelectCardEvent,
     SelectPieceEvent,
@@ -20,9 +22,64 @@ import {
     UpdateTimeEvent
 } from "./types";
 
+const BOARD_WIDTH = 5
+
 export const DEFAULT_POSITION: Position = {
     a1: "wP", b1: "wP", c1: "wK", d1: "wP", e1: "wP",
     a5: "bP", b5: "bP", c5: "bK", d5: "bP", e5: "bP"
+}
+
+function getDelta(options: number[]) {
+    const delta = [] as Delta[]
+    for (let i = 0; i < options.length; i++) {
+        const opt = options[i]
+        const y = Math.floor(opt / BOARD_WIDTH) - 2
+        const x = (opt % BOARD_WIDTH) - 2
+        delta.push({ x, y })
+    }
+    return { options, delta }
+}
+
+export const ALL_CARDS: CardType[] = [
+    { name: "tiger", ...getDelta([2, 17]) },
+    { name: "cobra", ...getDelta([8, 11, 18]) },
+    { name: "dragon", ...getDelta([5, 9, 16, 18]) },
+    { name: "rabbit", ...getDelta([8, 14, 16]) },
+    { name: "crab", ...getDelta([7, 10, 14]) },
+    { name: "elephant", ...getDelta([6, 8, 11, 13]) },
+    { name: "frog", ...getDelta([6, 10, 18]) },
+    { name: "goose", ...getDelta([6, 11, 13, 18]) },
+    { name: "rooster", ...getDelta([8, 11, 13, 16]) },
+    { name: "monkey", ...getDelta([6, 8, 16, 18]) },
+    { name: "mantis", ...getDelta([6, 8, 17]) },
+    { name: "ox", ...getDelta([7, 13, 17]) },
+    { name: "horse", ...getDelta([7, 11, 17]) },
+    { name: "crane", ...getDelta([7, 16, 18]) },
+    { name: "boar", ...getDelta([7, 11, 13]) },
+    { name: "eel", ...getDelta([6, 13, 16]) },
+]
+
+function shuffleCards(allCards: CardType[], numberOfCards: number) {
+    const tempDeck = [...allCards]
+    let deck = [] as CardType[]
+    for (let i = 0; i < numberOfCards && tempDeck.length > 0; i++) {
+        const selectedIdx = Math.floor(Math.random() * tempDeck.length)
+        const card = tempDeck[selectedIdx]
+        deck.push(card)
+        tempDeck.splice(selectedIdx, 1)
+    }
+    const wCards = deck.splice(0, 2)
+    const bCards = deck.splice(0, 2)
+
+    if (deck.length < 1 || deck.length > allCards.length || wCards.length !== 2 || bCards.length !== 2) {
+        throw new Error("card shuffling error!")
+    }
+
+    return [
+        wCards as [CardType, CardType],
+        bCards as [CardType, CardType],
+        deck
+    ] as const
 }
 
 export function updateBoard(board: Position, from: SquareType, to: SquareType): Position {
@@ -44,6 +101,7 @@ export function swapWithDeck(
     mutablePlayerCards.push(replacedCard)
     return mutablePlayerCards
 }
+
 export function getCardOptions(
     sourceSquare: SquareType,
     deltaOptions: Delta[],
@@ -69,6 +127,41 @@ export function getCardOptions(
     }
     return options
 }
+
+export function getPlayersForPlayOffline(): Record<"self" | "opponent", PlayerResponse> {
+    let playersInfo = {} as Record<"self" | "opponent", PlayerResponse>
+    playersInfo.self = { socketId: "111", userId: "111", username: "You" }
+    playersInfo.opponent = { socketId: "999", userId: "999", username: "Computer" }
+    return playersInfo
+}
+
+export function NewGameWithComputer(players: Record<"self" | "opponent", PlayerResponse>, gameInfo: GameInfo) {
+    const turnColor = Math.random() > .5 ? "w" : "b"
+    const { self, opponent } = players;
+    const whiteId = Math.random() > .5 ? self.socketId : opponent.socketId
+    const blackId = whiteId === self.socketId ? opponent.socketId : self.socketId
+    const turnId = turnColor === "w" ? whiteId : blackId
+    const boardPosition = DEFAULT_POSITION
+    const [wCards, bCards, deck] = shuffleCards(ALL_CARDS, gameInfo.numberOfCards)
+    const whiteCards = wCards
+    const blackCards = bCards
+    const gameTime = gameInfo.time
+    const [selfCards, opponentCards] =
+        self.socketId === whiteId
+            ? [whiteCards, blackCards]
+            : [blackCards, whiteCards]
+    return {
+        id: "999",
+        boardPosition,
+        selfColor: self.socketId === whiteId ? "w" : "b" as BlackOrWhite,
+        hasTurn: self.socketId === turnId,
+        selfCards: selfCards as [CardType, CardType],
+        opponentCards: opponentCards as [CardType, CardType],
+        time: gameTime,
+        deck
+    }
+}
+
 export const startGame = assign((_, e) => {
     return {
         gameId: e.id,
@@ -285,7 +378,6 @@ export const navigateBack = assign((ctx, _) => {
 }) as AssignAction<GameContext, NavigateBackEvent>
 
 export const navigateForward = assign((ctx, _) => {
-
     const { currentHistory, history } = ctx
     const updatedCurrentHistory = currentHistory + 1
     if (updatedCurrentHistory >= history.length) return ctx
--- src/lib/play/use-play-with-computer.ts
+++ src/lib/play/use-play-with-computer.ts
@@ -1,7 +1,38 @@
+import { useSelector } from "@xstate/react";
+import { useEffect, useState } from "react";
+import { getPlayersForPlayOffline, NewGameWithComputer } from "./consts";
+import { CardType, SquareType } from "./types";
 import { PlayImp, PlayArgs } from "./use-play-online";
 
-export default function usePlayWithComputer(args: PlayArgs): PlayImp {
-    const ryujinService = args.ryujinService
+export default function usePlayWithComputer({ ryujinService, gameInfo }: PlayArgs): PlayImp {
     const { send } = ryujinService
-    return {} as PlayImp
+    const isWaitingForComputer = useSelector(ryujinService, (state) => state.matches('lobby.waitingForComputer'))
+    const [deckCards, setDeckCards] = useState<CardType[]>([])
+
+    useEffect(() => {
+        if (!isWaitingForComputer) return;
+        const players = getPlayersForPlayOffline()
+        send({ type: "UPDATE_PLAYERS", players, roomId: "999" })
+        const newGame = NewGameWithComputer(players, gameInfo)
+        const { selfCards, boardPosition, id, deck, time, hasTurn, selfColor, opponentCards } = newGame
+        setDeckCards(deck)
+        send({
+            type: "GAME_STARTED",
+            id,
+            boardPosition,
+            hasTurn,
+            selfColor,
+            selfCards,
+            opponentCard: opponentCards,
+            time
+        })
+    }, [isWaitingForComputer])
+
+    function onMove(from: SquareType, to: SquareType, selectedCard: CardType) { }
+    function onPassTurn() { }
+    function onClaimOpponentTimeout() { }
+    function onResign() { }
+    function onCancelJoin() { }
+
+    return { onMove, onPassTurn, onClaimOpponentTimeout, onResign, onCancelJoin } as PlayImp
 }
